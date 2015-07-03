74f33f43ce7d783d88150df45ce8aaea3fb7ca84
yianyue <yianyue@gmail.com>
2015-07-03 18:09:14 -0400

    created ruby file

---
+++ pop_bottle.rb
@@ -0,0 +1,6 @@
+# Rules:
+# For every two empty bottles, you can get one free (full) bottle of pop
+# For every four bottle caps, you can get one free (full) bottle of pop
+# Each bottle of pop costs $2 to purchase
+
+#Given these parameters, write a program so that you can figure out how many total bottles of pop can be redeemed given a customer investment.
